887364342318506ab02f7fe8ca4c7ac96ff2b8a9
RuanSathler <ruansathler021@gmail.com>
2024-11-17 14:33:17 -0400

    versao 2.0.0

--- trabalho/registro.h
+++ trabalho/registro.h
@@ -1,9 +1,7 @@
 #ifndef REGISTRO_H
 #define REGISTRO_H
 
-#include <stdio.h>
-#include "campoMinado.h"
-
+typedef struct celulaCampoMinado celula;
 
 // CriaArquivo: Função responsável por criar um arquivo para salvar o histórico do jogo.
 // O nome do arquivo é gerado com base na data e hora atual, no formato "historico/dd-mm-yy HH-MM".
@@ -11,10 +9,9 @@
 FILE *CriaArquivo();
 
 // SalvaJogada: Função que registra o estado do jogo em um arquivo após cada jogada.
-void SalvaJogada();
+void SalvaJogada(int cooL, int cooC, int espacosRestantes,  celula **mapa, int tamanho, FILE *arquivo);
 
 // SalvaFimJogo: Registra o estado final do jogo no arquivo, incluindo o resultado (vitória ou derrota).
-// Parâmetros:
 void SalvaFimJogo(int cooL, int cooC, int espacosRestantes, celula **mapa, int tamanho, FILE *arquivo, int vitoria);
 
 // FecharArquivo: Função responsável por fechar o arquivo após o término do jogo ou jogadas.
